3b1659d8920cfb4fd3747c6b138ea3e212bfe57a
Yimin2 <gownfla14@naver.com>
2023-10-25 15:44:33 +0900

    if 문 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,10 +6,12 @@ public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
         System.out.print("명령) ");
-
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
-
-        System.out.printf("입력받은 명령어 : %s\n", cmd);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            if (cmd.equals("종료"))
+                break;
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+        }
     }
 }
