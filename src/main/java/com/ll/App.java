c7932ef64b2dded90a8dd681439e3e379c56b0ab
Yimin2 <gownfla14@naver.com>
2023-10-26 13:36:47 +0900

    목록 생성

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,17 +1,46 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
+
+        int lastQuotationId = 0;
+        List<Quotation> quotations = new ArrayList<>();
+
         while (true) {
+            System.out.print("명령) ");
+
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
-            if (cmd.equals("종료"))
+
+            if (cmd.equals("종료")) {
                 break;
-            System.out.printf("입력받은 명령어 : %s\n", cmd);
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                lastQuotationId++;
+                int id = lastQuotationId;
+
+                Quotation quotation = new Quotation(id, content, authorName);
+                quotations.add(quotation);
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
+            }
+            else if (cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                for (int i = quotations.size() - 1; i>=0; i--){
+                    Quotation quotation = quotations.get(i);
+                    System.out.printf("%d / %s / %s\n",quotation.id, quotation.authorName, quotation.content);
+                }
+            }
         }
     }
 }
